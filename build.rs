e1b5511d7a37df43e40ef9373470eb1dca352552
Lucas S. Vieira <lucasvieira@protonmail.com>
2023-05-02 00:43:04 -0300

    Remove code smells in build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,3 @@
-use chrono;
 use std::{
     env,
     error::Error,
@@ -34,14 +33,14 @@ fn get_git_version() -> Result<String, Box<dyn Error>> {
     let tag_short = Command::new("git")
         .arg("rev-parse")
         .arg("--short")
-        .arg(&latest_tag)
+        .arg(latest_tag)
         .output()?;
     let tag_short = str::from_utf8(&tag_short.stdout)?.trim_end();
 
     if tag_short == commit_short {
         Ok(String::new()) // Favor version at Cargo.toml
     } else {
-        Ok(String::from(format!("{}-{}", commit_short, branch_name)))
+        Ok(format!("{}-{}", commit_short, branch_name))
     }
 }
 
@@ -51,17 +50,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let timestamp = chrono::offset::Local::now();
     let timestamp = timestamp.format("%Y-%m-%d %H:%M:%S");
     let destpath = Path::new(&outdir).join("timestamp.txt");
-    let mut file = BufWriter::new(File::create(&destpath)?);
+    let mut file = BufWriter::new(File::create(destpath)?);
     write!(file, "{}", timestamp)?;
 
     let version = get_git_version()?;
     let destpath = Path::new(&outdir).join("version.txt");
-    let mut file = BufWriter::new(File::create(&destpath)?);
+    let mut file = BufWriter::new(File::create(destpath)?);
     write!(file, "{}", version)?;
 
     let target = env::var("TARGET")?;
     let destpath = Path::new(&outdir).join("target.txt");
-    let mut file = BufWriter::new(File::create(&destpath)?);
+    let mut file = BufWriter::new(File::create(destpath)?);
     write!(file, "{}", target)?;
 
     Ok(())
